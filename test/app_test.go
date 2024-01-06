a74f2598ff7c3f3372382b03016cee2ceb246d86
farewell12345 <1149558764@qq.com>
2024-01-06 20:48:58 +0800

    feat: bot init

--- test/app_test.go
+++ test/app_test.go
@@ -2,10 +2,9 @@ package test
 
 import (
 	conf "bot-faq-qq/config"
+	"bot-faq-qq/internal/pkg/handler"
 	"context"
-	"fmt"
 	"github.com/tencent-connect/botgo"
-	"github.com/tencent-connect/botgo/dto"
 	"github.com/tencent-connect/botgo/token"
 	"github.com/tencent-connect/botgo/websocket"
 	"log"
@@ -14,21 +13,41 @@ import (
 )
 
 func TestBot(t *testing.T) {
-	token := token.BotToken(conf.AppID, conf.Token)
-	api := botgo.NewOpenAPI(token).WithTimeout(3 * time.Second)
+	botToken := token.BotToken(conf.AppID, conf.Token)
+	//api := botgo.NewOpenAPI(token).WithTimeout(3 * time.Second)
+	// 沙箱环境
+	api := botgo.NewSandboxOpenAPI(botToken).WithTimeout(3 * time.Second)
 	ctx := context.Background()
 	ws, err := api.WS(ctx, nil, "")
 	if err != nil {
 		log.Printf("%+v, err:%v", ws, err)
 	}
 
-	// 监听哪类事件就需要实现哪类的 handler，定义：websocket/event_handler.go
-	var atMessage = func(event *dto.WSPayload, data *dto.WSATMessageData) error {
-		fmt.Println(event, data)
-		return nil
+	process := handler.Processor{Api: api}
+	intent := websocket.RegisterHandlers(
+		// at 机器人事件，目前是在这个事件处理中有逻辑，会回消息，其他的回调处理都只把数据打印出来，不做任何处理
+		process.ATMessageEventHandler(),
+		// 如果想要捕获到连接成功的事件，可以实现这个回调
+		process.ReadyHandler(),
+		// 连接关闭回调
+		process.ErrorNotifyHandler(),
+		// 频道事件
+		process.GuildEventHandler(),
+		// 成员事件
+		process.MemberEventHandler(),
+		// 子频道事件
+		process.ChannelEventHandler(),
+		//// 私信，目前只有私域才能够收到这个，如果你的机器人不是私域机器人，会导致连接报错，那么启动 example 就需要注释掉这个回调
+		//process.DirectMessageHandler(),
+		//// 频道消息，只有私域才能够收到这个，如果你的机器人不是私域机器人，会导致连接报错，那么启动 example 就需要注释掉这个回调
+		//process.CreateMessageHandler(),
+		// 互动事件
+		process.InteractionHandler(),
+		// 发帖事件
+		process.ThreadEventHandler(),
+	)
+	// 指定需要启动的分片数为 2 的话可以手动修改 wsInfo
+	if err = botgo.NewSessionManager().Start(ws, botToken, &intent); err != nil {
+		log.Fatalln(err)
 	}
-	intent := websocket.RegisterHandlers(atMessage)
-	// 启动 session manager 进行 ws 连接的管理，如果接口返回需要启动多个 shard 的连接，这里也会自动启动多个
-	botgo.NewSessionManager().Start(ws, token, &intent)
-
 }
